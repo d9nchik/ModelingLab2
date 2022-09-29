97bf35653d32bef6fc4b51377ad04502eaaee9b9
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-09-29 17:24:22 +0300

    add clientStatus to Create

--- src/Create.java
+++ src/Create.java
@@ -7,8 +7,15 @@ public class Create extends Element {
     @Override
     public void outAct() {
         super.outAct();
-        super.inAct();
+        super.inAct(0);
         super.setTnext(super.getTcurr() + super.getDelay());
-        super.getNextElement().inAct();
+        double random = Math.random();
+        if (random < 0.5) {
+            super.getNextElement().inAct(1);
+        } else if (random < 0.6) {
+            super.getNextElement().inAct(2);
+        } else {
+            super.getNextElement().inAct(3);
+        }
     }
 }

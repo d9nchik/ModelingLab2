e072ee969fda1b09153c57cc9d50b3dbedf1c582
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-10-15 20:36:24 +0300

    done task 4.2

--- src/SimModel.java
+++ src/SimModel.java
@@ -2,10 +2,21 @@ import java.util.ArrayList;
 
 public class SimModel {
     public static void main(String[] args) {
-        ArrayList<Element> list = getSMO(10);
-        Model model = new Model(list);
-        model.setInfoPrinted(false);
-        model.simulate(1000);
+
+        for (int i = 10000; i < 100000; i += 10000) {
+            ArrayList<Element> list = getSMO(i - 1);
+
+            long startTime = System.currentTimeMillis();
+
+            Model model = new Model(list);
+            model.setInfoPrinted(false);
+            model.simulate(1000);
+
+            long endTime = System.currentTimeMillis();
+
+            long duration = (endTime - startTime);
+            System.out.printf("%5d\t%d\n", i, duration);
+        }
     }
 
     private static ArrayList<Element> getSMO(int N) {
